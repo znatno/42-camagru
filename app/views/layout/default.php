eda4e243128907ceab5d8acf38dffb48df8d0e63
znatno <ivanbohuna@gmail.com>
2020-05-12 03:50:30 +0300

    [56] - starting with layout

--- app/views/layout/default.php
+++ app/views/layout/default.php
@@ -12,34 +12,44 @@ if (isset($_SESSION['user'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8"/>
     <title><?= $title; ?></title>
     <link type="text/css" rel="stylesheet" href="/pub/css/stylesheet.css">
     <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<?php require 'app/lib/put_scripts.php' ?>
-
 </head>
+
 <body>
 <header>
-    <nav class="navbar navbar-expand navbar-dark bg-dark">
-
-        <a class="navbar-brand" href="/">Camagru</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-supported-content" aria-controls="navbar-supported-content" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="navbar-collapse" id="navbar-supported-content">
-            <ul class="navbar-nav mr-auto">
-				<?= $check_post ?>
-            </ul>
-            <ul class="navbar-nav my-2 my-lg-0">
-				<?= $check_login ?>
-            </ul>
+    <div class="nav">
+        <div class="container">
+            <div class="row">
+                <div class="col-4 col-md-2">
+                    <a href="/"><img src="/pub/res/img/logo.svg" alt="Logo"></a>
+                </div>
+                <div class="col-4 col-md-4 p-0 nav-wrapper">
+                    <div class="nav-links">
+                        <a class="nav-links__item" href="/">Feed</a>
+						<?= isset($_SESSION['user']) ? '<a href="/create/new" class="nav-links__item">New Post</a>' : '' ?>
+                    </div>
+                </div>
+                <div class="col-4 ml-auto p-0 nav-wrapper">
+                    <div class="nav-links float-right">
+						<?= isset($_SESSION['user'])
+                            ? '<a class="nav-links__item" href="/account/profile">'.$_SESSION['user']['username'].'</a>
+                                <a class="nav-links__item" href="/account/logout">Log Out</a>'
+                            : '<a class="nav-links__item" href="/account/login">Log In</a>
+                                <a class="nav-links__item" href="/account/register">Sign Up</a>'
+                        ?>
+                    </div>
+                </div>
+            </div>
         </div>
-
-    </nav>
+    </div>
 </header>
+
 <main role="main">
     <div id="snackbar">{Put text into the function}</div>
 	<?= $content; ?>
